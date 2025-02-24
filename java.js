235ec21fc00a4cecbcee1efdb5076824600d86a1
Joe <82463730+joeparadiso@users.noreply.github.com>
2025-02-23 23:08:12 -0500

    Update java.js
    Updated the way that the 12 hour clock is displayed

--- java.js
+++ java.js
@@ -13,12 +13,16 @@ function clock(){
     + today.getDate() + ", " 
     +today.getFullYear());
     
-    // Remove the % 12 for 24 hr time
-    var h = today.getHours() % 12;
+    // Uncomment line below and comment the following line for 24 hr clock
+    // var h = today.getHours();
+    var h = (today.getHours() !== 12) ? (today.getHours() % 12) : (today.getHours());
     var m = today.getMinutes();
     var s = today.getSeconds();
 
+    // Not implemented in UI: Determine whether the time is 'AM' or 'PM'
     var day = h<11 ? 'AM': 'PM';
+
+    // If the hour/min/sec is less than 10 (a single digit), add a preceding '0'
     h = h<10? '0'+h: h;
     m = m<10? '0'+m: m;
     s = s<10? '0'+s: s;
